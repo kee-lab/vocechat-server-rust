56d0647f22ac3e2c94f4c849e414a4708b6c837f
wy <wy@gmail.com>
2023-09-28 18:43:35 +0800

    debug

--- migrations/20230928093903_twitter_user.sql
+++ migrations/20230928093903_twitter_user.sql
@@ -4,5 +4,5 @@ CREATE TABLE twitter_user
     uid                 integer primary key autoincrement not null,
     twitter_id                  integer not null,
     username            text collate nocase not null,
-    profile_image_url   text,
+    profile_image_url   text
 )
