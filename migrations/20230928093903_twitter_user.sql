c433280e82eb73cb6ab8f165b9b5274799466fa0
wy <wy@gmail.com>
2023-09-28 18:28:15 +0800

    add table twitter_user

---
+++ migrations/20230928093903_twitter_user.sql
@@ -0,0 +1,8 @@
+-- Add migration script here
+CREATE TABLE twitter_user
+(
+    uid                 integer primary key autoincrement not null,
+    twitter_id                  integer not null,
+    username            text collate nocase not null,
+    profile_image_url   text,
+)
